aeac8a0c6f9d7ccb10e728162f45a86051a9f3f0
HetRed31 <sanapirat@gmail.com>
2025-12-09 21:36:36 +0300

    6_3 not full

---
+++ woof_6_3/src/main/java/com/example/woof_6_3/ui/theme/Type.kt
@@ -0,0 +1,35 @@
+package com.example.woof_6_3.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val AbrilFatface = FontFamily.Default
+
+val Montserrat = FontFamily.Default
+
+val Typography = Typography(
+    displayLarge = TextStyle(
+        fontFamily = AbrilFatface,
+        fontWeight = FontWeight.Normal,
+        fontSize = 36.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    )
+)

0af3769da464e4a15f5ab40a48563981fb560465
HetRed31 <sanapirat@gmail.com>
2025-12-23 22:59:09 +0300

    7_4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+ext {
+    set("lifecycle_version", "2.7.0")
+}
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
